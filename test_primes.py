4d66aa9d94c5d3a0c95ec94e9acf885f1b559714
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-29 07:53:19 +0200

    Add passing test for `test_primes`: `test_is_four_non_prime`

--- test_primes.py
+++ test_primes.py
@@ -9,6 +9,10 @@ class PrimesTestCase(unittest.TestCase):
         """Is five successfully determined to be prime?"""
         self.assertTrue(is_prime(5))
 
+    def test_is_four_non_prime(self):
+        """Is four correctly determined not to be prime?"""
+        self.assertFalse(is_prime(4), msg='Four is not prime!')
+
 
 if __name__ == '__main__':
     unittest.main()
