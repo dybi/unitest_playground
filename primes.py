81ac878b967ee56c82c079fd4f4bb4988af700de
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-29 07:55:12 +0200

    Fix `is_prime`: 0 and 1 are not primes

--- primes.py
+++ primes.py
@@ -1,5 +1,8 @@
 def is_prime(number):
     """Return True if *number* is prime."""
+    if number in (0, 1):
+        return False
+
     for element in range(2, number):
         if number % element == 0:
             return False
