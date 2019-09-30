8dfcf89e248eeb25daec5cb10b01c9b6b7232953
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-30 05:12:10 +0200

    Change the test for `give_me_the_money` to use mocks

--- test_mocks.py
+++ test_mocks.py
@@ -1,14 +1,15 @@
 from unittest import TestCase
+from unittest.mock import Mock
 
 from entities import Account, User
 
 
 class TestUser(TestCase):
     def test_give_me_the_money_should_return_available_funds_from_all_accounts(self):
-        account_1 = Account()
-        account_1.add_funds(50)
-        account_2 = Account()
-        account_2.add_funds(100)
+        account_1 = Mock()
+        account_1.get_balance.return_value = 50
+        account_2 = Mock()
+        account_2.get_balance.return_value = 100
 
         user = User("Janusz", 40, [account_1, account_2])
 
