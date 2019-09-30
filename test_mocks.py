eb9b3cea59b393e9276b60d6e46278de7956a8f3
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-30 05:25:21 +0200

    Add test for `Account.transfer` with mock (assert method calls)

--- test_mocks.py
+++ test_mocks.py
@@ -14,3 +14,14 @@ class TestUser(TestCase):
         user = User("Janusz", 40, [account_1, account_2])
 
         self.assertEqual(user.give_me_the_money(), 150)
+
+
+class TestAccount(TestCase):
+    def test_transfer_should_send_money_to_other_account(self):
+        account = Account()
+        other_account = Mock()
+
+        yogurt_price = 1.29
+        account.transfer(other_account, yogurt_price)
+
+        other_account.add_funds.assert_called_once_with(yogurt_price)
