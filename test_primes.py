461783d1900930ec40e0cf02667e64f887975974
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2019-09-30 07:38:09 +0200

    Change `test_primes` to be parametric tests

--- test_primes.py
+++ test_primes.py
@@ -1,24 +1,32 @@
 import unittest
+
+from parameterized import parameterized
+
 from primes import is_prime
 
 
 class PrimesTestCase(unittest.TestCase):
     """Tests for `primes.py`."""
 
-    def test_is_five_prime(self):
-        """Is five successfully determined to be prime?"""
-        self.assertTrue(is_prime(5))
+    @parameterized.expand([
+        (2,),
+        (3,),
+        (5,),
+        (7,),
+        (11,),
+    ])
+    def test_is_prime(self, number):
+        self.assertTrue(is_prime(number))
 
-    def test_is_four_non_prime(self):
-        """Is four correctly determined not to be prime?"""
-        self.assertFalse(is_prime(4), msg='Four is not prime!')
-
-    def test_is_zero_not_prime(self):
-        """Is zero correctly determined not to be prime?"""
-        self.assertFalse(is_prime(0))
-
-    def test_negative_is_not_prime(self):
-        self.assertFalse(is_prime(-1))
+    @parameterized.expand([
+        (4,),
+        (6,),
+        (9,),
+        (0,),
+        (-1,),
+    ])
+    def test_is_non_prime(self, number):
+        self.assertFalse(is_prime(number))
 
 
 if __name__ == '__main__':
